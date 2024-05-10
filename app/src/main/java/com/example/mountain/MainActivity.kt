a762eed5fb8ce805c4e860eb66ece19081281167
Maksym <maks.pata@gmail.com>
2024-05-10 21:38:33 +0200

    Add hamburger menu

--- app/src/main/java/com/example/mountain/MainActivity.kt
+++ app/src/main/java/com/example/mountain/MainActivity.kt
@@ -8,20 +8,39 @@ import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
 import android.view.View
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.ActionBarDrawerToggle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         val actionBar = findViewById<Toolbar>(R.id.action_bar)
         setSupportActionBar(actionBar)
         actionBar.title = getString(R.string.app_name)
+
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        val toggle =  ActionBarDrawerToggle(
+            this,
+            drawer,
+            actionBar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+
+        drawer.addDrawerListener(toggle)
+
+        toggle.syncState()
     }
 
     fun onShowDetail(view: View) {
