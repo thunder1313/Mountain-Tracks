1d098055c3841cc9fa6e758ff5335e3b2d9572ee
janodwazny <jan.odwazny.g4@gmail.com>
2024-05-13 18:24:58 +0200

    added animation for tablet and normal layout

--- app/src/main/java/com/example/mountain/MainActivity.kt
+++ app/src/main/java/com/example/mountain/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.mountain
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
@@ -17,6 +25,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Handler(Looper.getMainLooper()).postDelayed({
+            Animation()
+        }, 1500)
+//        Animation()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -47,4 +59,32 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(applicationContext, DetailActivity::class.java);
         startActivity(intent);
     }
-}
+
+    fun Animation(){
+        val animationView = findViewById<ImageView>(R.id.start_animation)
+
+        val spinAnimator = ObjectAnimator.ofFloat(animationView, "rotation", 0f, 360f)
+        spinAnimator.duration = 2000 // 2 seconds for one rotation
+        spinAnimator.repeatCount = ObjectAnimator.INFINITE
+        spinAnimator.interpolator = AccelerateInterpolator()
+
+        // Scale animation
+        val scaleAnimatorX = ObjectAnimator.ofFloat(animationView, "scaleX", 1f, 0f)
+        val scaleAnimatorY = ObjectAnimator.ofFloat(animationView, "scaleY", 1f, 0f)
+        scaleAnimatorX.duration = 2000
+        scaleAnimatorY.duration = 2000
+        scaleAnimatorX.interpolator = AccelerateInterpolator()
+        scaleAnimatorY.interpolator = AccelerateInterpolator()
+
+        // Start both animations together
+        spinAnimator.start()
+        scaleAnimatorX.start()
+        scaleAnimatorY.start()
+
+        // Hide the imageView when animation ends
+        spinAnimator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                animationView.visibility = View.GONE
+    }
+})
+    }}
